0555480be986590f72d56d5763b6093840f5ba58
Björn Dahlgren <bjorn@dahlgren.at>
2013-04-19 00:54:28 +0200

    Basic login page design

--- IRCCloud/LoginPage.xaml.cs
+++ IRCCloud/LoginPage.xaml.cs
@@ -20,5 +20,10 @@ namespace IRCCloud
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
